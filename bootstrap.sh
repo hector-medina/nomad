cfb0a70c05ac95725942919248c40b340625b7a2
Hector Medina <hector.medina.cabanelas@gmail.com>
2023-02-26 22:42:37 +0100

    add ansible to path

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,9 @@ apt update
 apt upgrade -y
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+python3 get-pip.py --user
 # Install ansible
-python3 -m pip install ansible
+python3 -m pip install --user ansible
+echo 'export PATH="$PATH:~/.local/bin"' >> ~/.bashrc
 # Run playbook
 ansible-pull https://github.com/hector-medina/nomad.git nomad.yml
