79aaa7c595734484ce1f12b0dd4a21df8425ba3e
Hector Medina <hector.medina.cabanelas@gmail.com>
2023-02-26 22:29:28 +0100

    remove user

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ apt update
 apt upgrade -y
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
+python3 get-pip.py
 # Install ansible
-python3 -m pip install --user ansible
+python3 -m pip install ansible
 # Run playbook
 ansible-pull https://github.com/hector-medina/nomad.git nomad.yml
