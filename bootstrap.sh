90ad66afe72b951c69a4fa0d3eae724d324c3e12
Hector Medina <hector.medina.cabanelas@gmail.com>
2023-02-26 22:10:08 +0100

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt update
+apt upgrade -y
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+# Install ansible
+python3 -m pip install --user ansible
+# Run playbook
+ansible-pull https://github.com/hector-medina/nomad.git nomad.yml
